2b2f893060d2e535f9bc0cc10a304c69b45512ec
w0rmr <toukmati2000@gmail.com>
2024-12-01 17:23:32 +0100

    some code

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,12 +1,12 @@
 #pragma once 
 #include <iostream>
- template <typename T>
- void iter(T* array ,int length, void (*fonc)( T&)){
+ template <typename T,typename N>
+ void iter(T* array ,int length, void (*fonc)( N&)){
     for(int i = 0 ;i < length;i++)
         fonc(array[i]);
  }
 
- template <typename T>
- void print(T& p){
-    std::cout << p << std::endl;
- }
+template <typename T>
+void print(T& p){
+   std::cout << p << std::endl;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -18,9 +18,9 @@
 int main() {
     std::cout << "Testing on array of integers:" << std::endl;
     int intArray[5] = {1, 2, 3, 4, 5};
-    iter(intArray, 5, print);
+    iter(intArray, 5, print<int>);
     std::cout << "Testing on array of strings:" << std::endl;
     std::string stringArray[3] = {"test1", "test2", "test3"};
-    iter(stringArray, 3, print);
+    iter(stringArray, 3, print<int>);
     return 0;
 }
